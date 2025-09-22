18f767c816d3c2a029a28e474a1a341fd28ee2f9
Confirm-cmd <munjericonfirm@gmail.com>
2025-09-22 15:55:35 +0200

    another cors

--- server.js
+++ server.js
@@ -36,7 +36,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS']
+})); 
 
 
 // Rate limiting
